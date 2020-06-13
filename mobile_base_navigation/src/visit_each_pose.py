c8a8ce01b6d862e114c1190ecc8c7f5969d72e92
Xiao Jiang <xjiang2@andrew.cmu.edu>
2020-06-13 16:38:33 -0400

    modify the visit eah spot function

--- mobile_base_navigation/src/visit_each_pose.py
+++ mobile_base_navigation/src/visit_each_pose.py
@@ -6,16 +6,19 @@ from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from actionlib_msgs.msg import GoalStatus
 from move_base_msgs.msg import MoveBaseActionGoal, MoveBaseAction, MoveBaseResult, MoveBaseGoal
+from std_srvs.srv import Empty
 import actionlib
 import time
 import os
 import rosparam
+import pandas as pd
+import rospkg
 
 
-class SendCoordinates(object):
-    def __init__(self, label):
-
-        client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+class Visit_each_pose(object):
+    def __init__(self, file_name):
+        self.visit_next = rospy.Service('/visit_next', Empty , self.visit_next_pose) 
+        self.client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
         rate = rospy.Rate(1)
 
         goal=MoveBaseGoal()
@@ -23,33 +26,55 @@ class SendCoordinates(object):
 
         self._ctrl_c = False
         rospy.on_shutdown(self.shutdownhook)
+        self.spots_file_name = file_name
+        #use position to store all the x and y in csv
+        self.poselist = []
+        self.read_csv()
+        self.index = 0
 
+    def read_csv(self):
+        # use rospack to get package info
+        rospack = rospkg.RosPack()
+        # We clean up spots folder
+        spots_folder_path = os.path.join(rospack.get_path('mobile_base_navigation'), "spots")
 
-        tag = label
+        csv_path = os.path.join(spots_folder_path,self.spots_file_name)
+        data = pd.read_csv(csv_path,names=['index','x','y'])
+        for i in range(len(data)):
+            self.poselist.append((data.iloc[i]['index'], data.iloc[i]['x'],data.iloc[i]['y']))
 
-        while not self._ctrl_c:
+    def visit_pose(self, x, y):
+        
+        goal=MoveBaseGoal()
+        goal_tmp = Pose()
+        goal_tmp.position.x=x
+        goal_tmp.position.y=y
+        goal_tmp.position.z=0.0
+        goal_tmp.orientation.x=0.0
+        goal_tmp.orientation.y=0.0
+        goal_tmp.orientation.z=1.0
+        goal_tmp.orientation.w=0.0
 
-            goal_tmp.position.x=rosparam.get_param(tag+'/x')
-            goal_tmp.position.y=rosparam.get_param(tag+'/y')
-            goal_tmp.position.z=0.0
-            goal_tmp.orientation.x=0.0
-            goal_tmp.orientation.y=0.0
-            goal_tmp.orientation.z=1.0
-            goal_tmp.orientation.w=0.0
+        goal.target_pose.pose=goal_tmp
+        goal.target_pose.header.frame_id='map'
 
-            goal.target_pose.pose=goal_tmp
-            goal.target_pose.header.frame_id='map'
+        self.client.wait_for_server()
+        self.client.send_goal(goal, feedback_cb=self.callback)
+        self.client.wait_for_result()
+        result=self.client.get_state()
+        if result==3:
+            print('successfuly reached point')
 
-            client.wait_for_server()
-            rospy.loginfo('Going to spot='+str(label))
-            client.send_goal(goal, feedback_cb=self.callback)
-            client.wait_for_result()
-            result=client.get_state()
-
-            #print result
-            if result==3:
-                print('successfuly reached point'+str(label))
-                self.shutdownhook()
+    def visit_next_pose(self, request):
+        self.index += 1
+        if self.index >= len(self.poselist):
+            # if finish all the spot, just ctrl_C
+            self.shutdownhook()
+        else:
+            x = self.poselist[self.index][1]
+            y = self.poselist[self.index][2]
+            rospy.loginfo('Going to spot='+str(self.poselist[self.index][0]))
+            self.visit_pose(x, y)
 
 
     def shutdownhook(self):
@@ -63,5 +88,5 @@ class SendCoordinates(object):
 
 if __name__ == "__main__":
     rospy.init_node('send_coordinates_node', log_level=rospy.INFO)
-    send_coordinates_object = SendCoordinates("pose0")
+    Visitor = Visit_each_pose("reorg_saved_spots2.csv")
     rospy.spin() # mantain the service open.
